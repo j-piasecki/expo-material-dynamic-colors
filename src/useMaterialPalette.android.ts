8e049456923bdba81124b5ff63d3a2ec281b52f8
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-08-09 21:17:24 +0200

    Diff palettes

--- src/useMaterialPalette.android.ts
+++ src/useMaterialPalette.android.ts
@@ -5,6 +5,23 @@ import { AppState } from 'react-native'
 
 export const materialYouSupported = MaterialColorsModule.supported
 
+function arePalettesEqual(
+  palette1: MaterialPalette | null,
+  palette2: MaterialPalette | null
+): boolean {
+  if (!palette1 || !palette2) {
+    return false
+  }
+
+  return (
+    palette1.systemAccent1.join(',') === palette2.systemAccent1.join(',') &&
+    palette1.systemAccent2.join(',') === palette2.systemAccent2.join(',') &&
+    palette1.systemAccent3.join(',') === palette2.systemAccent3.join(',') &&
+    palette1.systemNeutral1.join(',') === palette2.systemNeutral1.join(',') &&
+    palette1.systemNeutral2.join(',') === palette2.systemNeutral2.join(',')
+  )
+}
+
 export function useMaterialPalette(): MaterialPalette | null {
   const [materialYou, setMaterialYou] = useState<MaterialPalette | null>(
     MaterialColorsModule.getMaterialColors()
@@ -17,14 +34,17 @@ export function useMaterialPalette(): MaterialPalette | null {
 
     const subscription = AppState.addEventListener('change', (state) => {
       if (state === 'active') {
-        setMaterialYou(MaterialColorsModule.getMaterialColors())
+        const newPalette = MaterialColorsModule.getMaterialColors()
+        if (!arePalettesEqual(materialYou, newPalette)) {
+          setMaterialYou(newPalette)
+        }
       }
     })
 
     return () => {
       subscription.remove()
     }
-  }, [])
+  }, [materialYou])
 
   return materialYou
 }
